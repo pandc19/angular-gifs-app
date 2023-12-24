514c44e4a4cd0bada0eb1ec8e0474388eb58e73f
Jonathan Pillajo <pandc19@outlook.com>
2023-12-24 13:19:46 -0500

    Fin sección 7

---
+++ src/app/gifs/pages/home/home-page.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { GifsService } from '../../services/gitfs.service';
+import { Gif } from '../../interfaces/gifs.interfaces';
+
+@Component({
+  selector: 'gifs-home-page',
+  templateUrl: './home-page.component.html'
+})
+export class HomePageComponent {
+
+  constructor(private gifsService: GifsService) { }
+
+  get gifs(): Gif[] {
+    return this.gifsService.gifList;
+  }
+}
